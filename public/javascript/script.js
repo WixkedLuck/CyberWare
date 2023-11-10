e2b8de92ba0cd03c0fd3559a73d3daa47d528481
David-Hybner <dah1998@yahoo.com>
2023-11-10 10:07:10 -0500

    Email.js functional
    Fixed problems with the form not working with emailjs

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,10 +1,11 @@
-function SendMail() {
+function SendMail() { 
+
 var params = {
     from_name: document.getElementById("fullName").value,
     email_id: document.getElementById("email_id").value,
     message: document.getElementById("message").value,
 }
-emailjs.send("service_46w1h8g", "template_7axa13r", params).then(function(res){
-    alert("Success!" +res.status); 
+emailjs.send("service_46w1h8g", "template_7axa13r", params, "ZSWIAF0k4AttoBBVE").then(function(res){
+    alert("Success! Thank you for sending us a message we will be back to you soon!" +res.status); 
 })
 }
