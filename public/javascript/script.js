09e830021e2ce6a88ad31ef895c7cb63c2fbbfa3
David-Hybner <dah1998@yahoo.com>
2023-11-20 14:25:11 -0500

    added comments
    added comments for email.js file

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,10 +1,12 @@
 function SendMail() { 
 
 var params = {
+    //combining Email.js template we create params to get specific values from the contact form
     from_name: document.getElementById("fullName").value,
     email_id: document.getElementById("email_id").value,
     message: document.getElementById("message").value,
 }
+//send the params using your serivice id, templete id, param values, and public key 
 emailjs.send("service_46w1h8g", "template_7axa13r", params, "ZSWIAF0k4AttoBBVE").then(function(res){
     alert("Success! Thank you for sending us a message we will be back to you soon!" +res.status); 
 })
